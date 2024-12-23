49a0c22f59e881306b9d1d9127f009b76a906727
shash <drs.prabhu2018@gmail.com>
2024-12-23 20:31:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ThemeProvider } from './components/ThemeContext'
+import { ThemeProvider } from './components/Themecontext'
 
 createRoot(document.getElementById('root')).render(
   <ThemeProvider>
